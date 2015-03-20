5886886b595e6a814312f42480767ecf13b96239
uwo <onno3940@gmail.com>
2015-03-20 15:36:07 -0400

    add scm

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject org.clojars.uwo/react-bootstrap "0.13.0"
   :description "React Bootstrap packaged with Google Closure externs"
   :url "http://react-bootstrap.github.io/"
+  :scm "https://github.com/uwo/react-bootstrap"
   :license {:name "MIT" 
             :url "https://github.com/react-bootstrap/react-bootstrap/blob/master/LICENSE"})
